22922eca573f2a7e0a46927cb5604618a3850a01
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2024-12-22 19:58:18 +0800

    Update concak.lua

--- concak.lua
+++ concak.lua
@@ -15,12 +15,29 @@ local sitinkgui = sitinklib:Start({
 })
 local MainTab = sitinkgui:MakeTab("Main")
 
-local Section1 = MainTab:Section({
+local Ctio = MainTab:Section({
+    ["Title"] = "Credit / Info",
+    ["Content"] = ""
+})
+
+local Credit1 = Ctio:Paragraph({
+    ["Title"] = "Admin Script",
+    ["Content"] = "Name: Nhật Minh \ Age: 14 \ Vietnam"
+})
+
+local Credit2 = Ctio:Paragraph({
+    ["Title"] = "Script Support",
+    ["Content"] = "PC"
+})
+
+local Farming = MainTab:Section({
     ["Title"] = "Auto Farm",
     ["Content"] = ""
 })
 
-local Farming1 = Farming1:Toggle({
+local Farming0 = Farming:Seperator("「 Farming #1 」")
+
+local Farming1 = Farming:Toggle({
 	["Title"]= "Auto Grab",
 	["Content"] = "",
 	["Default"] = false,
@@ -29,7 +46,7 @@ local Farming1 = Farming1:Toggle({
 	end
 })
 
-local Farming2 = Farming2:Toggle({
+local Farming2 = Farming:Toggle({
 	["Title"]= "Auto Eat",
 	["Content"] = "",
 	["Default"] = false,
@@ -38,7 +55,7 @@ local Farming2 = Farming2:Toggle({
 	end
 })
 
-local Farming3 = Farming3:Toggle({
+local Farming3 = Farming:Toggle({
 	["Title"]= "Auto Sell",
 	["Content"] = "",
 	["Default"] = false,
@@ -47,11 +64,145 @@ local Farming3 = Farming3:Toggle({
 	end
 })
 
-local Farming4 = Farming4:Toggle({
+local Farming4 = Farming:Toggle({
 	["Title"]= "Auto Throw",
 	["Content"] = "",
 	["Default"] = false,
 	["Callback"] = function(Value) 
 		print(Value)
 	end
+})
+
+local FarmingX0 = Farming:Seperator("「 Farming #2 」")
+
+local FarmingX1 = Farming:Toggle({
+	["Title"]= "Auto Tp Random",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
+})
+
+local FarmingX2 = Farming:Toggle({
+	["Title"]= "Auto Move",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
+})
+
+local FarmingX3 = Farming:Toggle({
+	["Title"]= "Auto Jump",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
+})
+
+local FarmingX4 = Farming:Toggle({
+	["Title"]= "Anti Ragdoll",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
+})
+
+local FarmingX5 = Farming:Toggle({
+	["Title"]= "Auto Collect Cube",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
+})
+
+local FarmingC0 = Farming:Seperator("「 Others 」")
+
+local FarmingC1 = Farming:Toggle({
+	["Title"]= "Auto Spin",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
+})
+
+local FarmingC2 = Farming:Toggle({
+	["Title"]= "Auto Rewards",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
+})
+
+local CW = MainTab:Section({
+    ["Title"] = "Config Wait",
+    ["Content"] = ""
+})
+
+local CW0 = CW:Seperator("「 Config Wait #1 」")
+
+local CW1 = CW:Slider({
+    ["Title"] = "Wait : Grab",
+    ["Content"] = "Time To Use Toggles",
+    ["Min"] = 0,
+    ["Max"] = 50,
+    ["Increment"] = 1,
+    ["Default"] = 0.1,
+    ["Callback"] = function(Value)
+        print(Value)
+    end
+})
+
+local CW2 = CW:Slider({
+    ["Title"] = "Wait : Eat",
+    ["Content"] = "Time To Use Toggles",
+    ["Min"] = 0,
+    ["Max"] = 50,
+    ["Increment"] = 1,
+    ["Default"] = 0.1,
+    ["Callback"] = function(Value)
+        print(Value)
+    end
+})
+
+local CW3 = CW:Slider({
+    ["Title"] = "Wait : Sell",
+    ["Content"] = "Time To Use Toggles",
+    ["Min"] = 0,
+    ["Max"] = 50,
+    ["Increment"] = 1,
+    ["Default"] = 0.1,
+    ["Callback"] = function(Value)
+        print(Value)
+    end
+})
+
+local CW4 = CW:Slider({
+    ["Title"] = "Wait : Throw",
+    ["Content"] = "Time To Use Toggles",
+    ["Min"] = 0,
+    ["Max"] = 50,
+    ["Increment"] = 1,
+    ["Default"] = 0.1,
+    ["Callback"] = function(Value)
+        print(Value)
+    end
+})
+
+local CW5 = CW:Slider({
+    ["Title"] = "Wait : TP Random",
+    ["Content"] = "Time To Use Toggles",
+    ["Min"] = 0,
+    ["Max"] = 50,
+    ["Increment"] = 1,
+    ["Default"] = 2.2,
+    ["Callback"] = function(Value)
+        print(Value)
+    end
 })
