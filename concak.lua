648609ac31715b08ea5c7b1b0268993cd3813449
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2024-12-22 18:45:47 +0800

    Update concak.lua

--- concak.lua
+++ concak.lua
@@ -7,104 +7,53 @@ local sitinkgui = sitinklib:Start({
     ["Logo Info"] = "rbxassetid://18243105495",
     ["Logo Player"] = "rbxassetid://18243105495",
     ["Name Info"] = "World Sjw Hub",
-    ["Name Player"] = "ruteteru",
-    ["Info Description"] = "discord.gg/3Aatp4Nhjp",
+    ["Name Player"] = "World Sjw Hub Community",
+    ["Info Description"] = "discord.gg/psE8EUa9kg",
     ["Tab Width"] = 140,
     ["Color"] = Color3.fromRGB(141,0,255,255),
     ["CloseCallBack"] = function() end
 })
 local MainTab = sitinkgui:MakeTab("Main")
+
 local Section = MainTab:Section({
-    ["Title"] = "Local Player",
-    ["Content"] = "Local Player Function"
-})
-local Seperator = Section:Seperator("Local Player")
-local Paragraph = Section:Paragraph({
-    ["Title"] = "Paragraph",
+    ["Title"] = "Auto Farm",
     ["Content"] = ""
 })
-local Paragraph1 = Section:Paragraph({
-    ["Title"] = "Paragraph",
-    ["Content"] = "This is a Paragraph"
-})
-local Button = Section:Button({
-    ["Title"] = "Button",
-    ["Content"] = "",
-    ["Callback"] = function()
-        print("Button Clicked!")
-    end
-})
-local Button1 = Section:Button({
-    ["Title"] = "Button",
-    ["Content"] = "This is a Button",
-    ["Callback"] = function()
-        print("Button Clicked!")
-    end
-})
-local TextInput = Section:TextInput({
-    ["Title"] = "Text Input",
-    ["Content"] = "",
-    ["Place Holder Text"] = "Enter you text here...",
-    ["Clear Text On Focus"] = true,
-    ["Callback"] = function(Value)
-        print(Value)
-    end
-})
-local TextInput1 = Section:TextInput({
-    ["Title"] = "Text Input",
-    ["Content"] = "This is a Text Input",
-    ["Place Holder Text"] = "Enter you text here...",
-    ["Clear Text On Focus"] = true,
-    ["Callback"] = function(Value)
-        print(Value)
-    end
-})
-local Toggle = Section:Toggle({
-	["Title"]= "Toggle",
+
+local Farming1 = Section:Toggle({
+	["Title"]= "Auto Grab",
 	["Content"] = "",
-	["Default"] = true,
-	["Callback"] = function(Value) 
-		print(Value)
-	end
-})
-local Toggle1 = Section:Toggle({
-	["Title"]= "Toggle",
-	["Content"] = "This is a Toggle",
 	["Default"] = false,
 	["Callback"] = function(Value) 
 		print(Value)
 	end
 })
-local Slider = Section:Slider({
-    ["Title"] = "Slider",
-    ["Content"] = "",
-    ["Min"] = 0,
-    ["Max"] = 100,
-    ["Increment"] = 1,
-    ["Default"] = 30,
-    ["Callback"] = function(Value)
-        print(Value)
-    end
+
+local Farming2 = Section:Toggle({
+	["Title"]= "Auto Eat",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
 })
-local Slider1 = Section:Slider({
-    ["Title"] = "Slider",
-    ["Content"] = "This is a Slider",
-    ["Min"] = 0,
-    ["Max"] = 100,
-    ["Increment"] = 1,
-    ["Default"] = 30,
-    ["Callback"] = function(Value)
-        print(Value)
-    end
+
+local Farming3 = Section:Toggle({
+	["Title"]= "Auto Sell",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
 })
-local Dropdown = Section:Dropdown({
-    ["Title"] = "Dropdown",
-    ["Multi"] = true,
-    ["Options"] = {"Option 1", "Option 2"},
-    ["Default"] = {"Option 1"},
-    ["Place Holder Text"] = "Select Options",
-    ["Callback"] = function(Value)
-        print(Value)
-    end
+
+local Farming4 = Section:Toggle({
+	["Title"]= "Auto Throw",
+	["Content"] = "",
+	["Default"] = false,
+	["Callback"] = function(Value) 
+		print(Value)
+	end
 })
+
 local ProfileTab = sitinkgui:MakeTab("Profile")
